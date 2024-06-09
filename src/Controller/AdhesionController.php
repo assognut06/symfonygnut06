015ca77e76d25fda2ca2a32322f4de8c209e6f16
Gerald Col <gnut@gnut.eu>
2024-06-09 03:13:34 +0200

    Amélioration Accueil messages flash

--- src/Controller/AdhesionController.php
+++ src/Controller/AdhesionController.php
@@ -5,15 +5,32 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Twig\Environment;
 
 class AdhesionController extends AbstractController
 {
+    private $twig;
+
+    public function __construct(Environment $twig)
+    {
+        $this->twig = $twig;
+    }
+
     #[Route('/adhesion', name: 'app_adhesion')]
     public function index(): Response
     {
-        return $this->render('adhesion/index.html.twig', [
+        // Get the current date
+        $currentDate = new \DateTime();
+
+        // Validate the date
+        if (!$currentDate) {
+            throw new \Exception('Failed to get the current date');
+        }
+
+        // Render the view
+        return new Response($this->twig->render('adhesion/index.html.twig', [
             'controller_name' => 'AdhesionController',
-            'currentDate' => new \DateTime(),
-        ]);
+            'currentDate' => $currentDate,
+        ]));
     }
 }
