522b19ff139b8c4b7be934f49e037993b609169b
laBHaZe06 <borne.yoan@outlook.fr>
2024-06-12 13:22:50 +0200

    update webpack configuration

---
+++ app/run.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+set -e
+
+# Installer les dépendances npm
+npm install
+
+# Démarrer le serveur Symfony
+symfony server:start --no-tls --port=8000 --dir=public &
+
+# Exécuter npm run watch en arrière-plan
+npm run watch
