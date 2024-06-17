5a5f492033182506525a7684f1bf8ff1885fdb26
Gerald Col <gnut@gnut.eu>
2024-06-17 19:45:44 +0200

    Mise en place de l'admin

--- app/src/Controller/AdminController.php
+++ app/src/Controller/AdminController.php
@@ -4,15 +4,37 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Service\HelloAssoAuthService;
 
+#[Route('/admin')]
 class AdminController extends AbstractController
 {
-    #[Route('/admin', name: 'admin_dashboard')]
+    private $helloAssoAuthService;
+
+    public function __construct(HelloAssoAuthService $helloAssoAuthService)
+    {
+        $this->helloAssoAuthService = $helloAssoAuthService;
+    }
+    #[Route('', name: 'admin_dashboard')]
     public function dashboard()
     {
-        // Votre logique d'administration ici
+        $bearerToken = $this->helloAssoAuthService->getToken();
+        $url = "https://api.helloasso.com/v5/organizations/" . $_ENV['SLUGASSO'];
+        $authorization = "Bearer " . $bearerToken;
 
-        return $this->render('admin/dashbord/dashboard.html.twig');
+        $client = new \GuzzleHttp\Client();
+
+        $response = $client->request('GET', $url, [
+            'headers' => [
+                'accept' => 'application/json',
+                'authorization' => $authorization,
+            ],
+        ]);
+        $data = json_decode($response->getBody(), true);
+
+        return $this->render('admin/dashbord/dashboard.html.twig', [
+            'data' => $data,
+        ]);
     }
 
     // Ajoutez d'autres méthodes au besoin
