48e0bc1e2bc25d766cd5de9b195fae87bb0ac4f4
Gerald Col <gnut@gnut.eu>
2024-06-19 22:32:13 +0200

    Amélioration du profil

--- app/src/Controller/AdminController.php
+++ app/src/Controller/AdminController.php
@@ -41,23 +41,23 @@ class AdminController extends AbstractController
     public function api(string $donnees, string $page, string $formType, string $formSlug, string $tierTypes)
     {
         if ($donnees === 'orders') {
-            $url = "https://api.helloasso.com/v5/organizations/" . $_ENV['SLUGASSO'] . "/items?pageIndex=" . $page . "&pageSize=15&withDetails=true&sortOrder=Desc&sortField=Date";
+            $url = "https://api.helloasso.com/v5/organizations/" . $_ENV['SLUGASSO'] . "/items?pageIndex=" . $page . "&pageSize=15&withDetails=true&sortOrder=Desc&sortField=Date&itemStates=Processed";
             if ($formType !== '1' && $formSlug !== '1') {
-                $url = "https://api.helloasso.com/v5/organizations/" . $_ENV['SLUGASSO'] . "/forms/" . $formType . "/" . $formSlug . "/items?pageIndex=" . $page . "&pageSize=15&withDetails=true&sortOrder=Desc&sortField=Date";
+                $url = "https://api.helloasso.com/v5/organizations/" . $_ENV['SLUGASSO'] . "/forms/" . $formType . "/" . $formSlug . "/items?pageIndex=" . $page . "&pageSize=15&withDetails=true&sortOrder=Desc&sortField=Date&itemStates=Processed";
             }
             if ($tierTypes !== '1') {
-                $url = "https://api.helloasso.com/v5/organizations/" . $_ENV['SLUGASSO'] . "/items?pageIndex=" . $page . "&pageSize=15&tierTypes=" . $tierTypes . "&withDetails=true&sortOrder=Desc&sortField=Date";
+                $url = "https://api.helloasso.com/v5/organizations/" . $_ENV['SLUGASSO'] . "/items?pageIndex=" . $page . "&pageSize=15&tierTypes=" . $tierTypes . "&withDetails=true&sortOrder=Desc&sortField=Date&itemStates=Processed";
                 // https://api.helloasso.com/v5/organizations/gnut-06/items?pageIndex=1&pageSize=15Donation&withDetails=true&sortOrder=Desc&sortField=Date
             }
         }
         if ($donnees === 'payments') {
-            $url = "https://api.helloasso.com/v5/organizations/" . $_ENV['SLUGASSO'] . "/payments?pageIndex=" . $page . "&pageSize=15&withDetails=true&sortOrder=Desc&sortField=Date";
+            $url = "https://api.helloasso.com/v5/organizations/" . $_ENV['SLUGASSO'] . "/payments?pageIndex=" . $page . "&pageSize=15&withDetails=true&sortOrder=Desc&sortField=Date&states=Authorized";
             
             // 'https://api.helloasso.com/v5/organizations/gnut-06/payments?pageIndex=1&pageSize=15&sortOrder=Desc&sortField=Date'
 
             if ($formType !== '1') {
                 // 'https://api.helloasso.com/v5/organizations/gnut-06/payments/search?pageSize=15&formType=Donation&sortOrder=Desc&sortField=Date';
-                $url = "https://api.helloasso.com/v5/organizations/" . $_ENV['SLUGASSO'] . "/payments/search?pageSize=15&formType=" . $formType . "&sortOrder=Desc&sortField=Date";
+                $url = "https://api.helloasso.com/v5/organizations/" . $_ENV['SLUGASSO'] . "/payments/search?pageSize=15&formType=" . $formType . "&sortOrder=Desc&sortField=Date&states=Authorized";
             }
         }
 
--- app/src/Controller/ProfilController.php
+++ app/src/Controller/ProfilController.php
@@ -9,7 +9,7 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 use App\Service\HelloAssoAuthService;
 use Symfony\Component\HttpFoundation\Request;
-
+#[Route('/profil')]
 class ProfilController extends AbstractController
 {
     private $helloAssoAuthService;
@@ -19,7 +19,7 @@ class ProfilController extends AbstractController
         $this->helloAssoAuthService = $helloAssoAuthService;
     }
 
-    #[Route('/profil', name: 'app_profil')]
+    #[Route('', name: 'app_profil')]
     public function index(Request $request): Response
     {
         // Assurez-vous que l'utilisateur est connecté
@@ -30,7 +30,7 @@ class ProfilController extends AbstractController
         $userEmail = urlencode($user->getUserIdentifier());
         $page = $request->query->get('page', 1);
         $bearerToken = $this->helloAssoAuthService->getToken();
-        $url="https://api.helloasso.com/v5/organizations/". $_ENV['SLUGASSO']  ."/items?userSearchKey=" . $userEmail . "&pageIndex=". $page . "&pageSize=4&withDetails=false&sortOrder=Desc&sortField=Date";
+        $url="https://api.helloasso.com/v5/organizations/". $_ENV['SLUGASSO']  ."/items?userSearchKey=" . $userEmail . "&pageIndex=". $page . "&pageSize=4&withDetails=false&sortOrder=Desc&sortField=Date&itemStates=Processed";
         $authorization = "Bearer " . $bearerToken;
 
         $client = new \GuzzleHttp\Client();
@@ -43,29 +43,38 @@ class ProfilController extends AbstractController
         ]);
         $data_items= json_decode($response->getBody(), true);
         // dump($user);
-
+        $googleMapsApiKey = $_ENV['GNUT06MAPAPI'];
         // exit;
         // Renvoyer à la vue Twig, en passant l'utilisateur comme variable
         return $this->render('profil/index.html.twig', [
             'user' => $user,
             'data_items' => $data_items,
+            'googleMapsApiKey' => $googleMapsApiKey,
         ]);
     }
 
-    #[Route('/profil/{page}', name: 'app_profil_page')]
-    public function page(string $page): Response
+    #[Route('/{donnees}/{page}', name: 'app_profil_page')]
+    public function page(string $page, string $donnees): Response
     {
         // Assurez-vous que l'utilisateur est connecté
         $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
         if ($page < 1) {
             $page = 1;
         }
+
         // Récupérer l'utilisateur connecté
         $user = $this->getUser();
         $userEmail = urlencode($user->getUserIdentifier());
         // $page = $request->query->get('page', 1);
+        if ($donnees === 'orders') {
+            $url="https://api.helloasso.com/v5/organizations/". $_ENV['SLUGASSO']  ."/items?userSearchKey=" . $userEmail . "&pageIndex=". $page . "&pageSize=4&withDetails=false&sortOrder=Desc&sortField=Date&itemStates=Processed";
+        } elseif ($donnees === 'payments') {
+            $url="https://api.helloasso.com/v5/organizations/". $_ENV['SLUGASSO']  ."/payments?userSearchKey=" . $userEmail . "&pageIndex=". $page . "&pageSize=4&withDetails=false&sortOrder=Desc&sortField=Date&states=Authorized";
+            https://api.helloasso.com/v5/organizations/gnut-06/payments?userSearchKey=contact%40cgmnice.com&pageIndex=1&pageSize=20&states=Authorized&sortOrder=Desc&sortField=Date
+        }
         $bearerToken = $this->helloAssoAuthService->getToken();
-        $url="https://api.helloasso.com/v5/organizations/". $_ENV['SLUGASSO']  ."/items?userSearchKey=" . $userEmail . "&pageIndex=". $page . "&pageSize=4&withDetails=false&sortOrder=Desc&sortField=Date";
+
+        
         $authorization = "Bearer " . $bearerToken;
 
         $client = new \GuzzleHttp\Client();
@@ -78,12 +87,13 @@ class ProfilController extends AbstractController
         ]);
         $data_items= json_decode($response->getBody(), true);
         // dump($user);
-
+        $googleMapsApiKey = $_ENV['GNUT06MAPAPI'];
         // exit;
         // Renvoyer à la vue Twig, en passant l'utilisateur comme variable
         return $this->render('profil/index.html.twig', [
             'user' => $user,
             'data_items' => $data_items,
+            'googleMapsApiKey' => $googleMapsApiKey,
         ]);
     }
 }
