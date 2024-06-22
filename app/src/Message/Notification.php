6d8bd06451614a98f12d170226ce4a570e034221
Gerald Col <gnut@gnut.eu>
2024-06-22 23:20:25 +0200

    Modif Notification.php

--- app/src/Message/Notification.php
+++ app/src/Message/Notification.php
@@ -2,15 +2,14 @@
 
 namespace App\Message;
 
-
 class Notification {
+    private string $content;
 
-    public function __construct(
-        private string $content,
-    ) {}
+    public function __construct(string $content) {
+        $this->content = $content;
+    }
 
-    public function getContent(): string
-    {
+    public function getContent(): string {
         return $this->content;
     }
 }
