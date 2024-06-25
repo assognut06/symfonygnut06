b42d7b5d36d006bfe6c79ce1bb263d8915a6f941
Gerald Col <gnut@gnut.eu>
2024-06-25 16:36:46 +0200

    Resolution envois Emails

--- app/src/Controller/EmailController.php
+++ app/src/Controller/EmailController.php
@@ -7,9 +7,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Mime\Email;
+use Symfony\Component\Routing\Annotation\Route;
 
 class EmailController extends AbstractController
 {
+    // #[Route('/email', name: 'send_email')]
     public function sendEmail(MailerInterface $mailer): Response
     {
         $email = (new Email())
@@ -19,8 +21,9 @@ class EmailController extends AbstractController
             ->text('Ceci est un test d\'envoi d\'email.')
             ->html('<p>Ceci est un test d\'envoi d\'email.</p>');
 
+        var_dump($email);
         $mailer->send($email);
 
-        return new Response('Email envoyé avec succès.');
+        return new Response("success", 200);
     }
 }
