fb84c21a04cb049ac905dccdfd622c48b6b98f71
Gerald Col <gnut@gnut.eu>
2024-06-25 18:28:42 +0200

    Mise en place de Boostrap 5.3 avec Webpack Co-authored-by: Yoan <laBHaZe06@users.noreply.github.com>

--- app/assets/app.js
+++ app/assets/app.js
@@ -5,7 +5,21 @@
  * This file will be included onto the page via the importmap() Twig function,
  * which should already be in your base.html.twig.
  */
-import './styles/app.css';
-import './bootstrap';
+import './styles/app.scss';
+// import './bootstrap';
+const $ = require('jquery');
+// this "modifies" the jquery module: adding behavior to it
+// the bootstrap module doesn't export/return anything
+require('bootstrap');
+require('bootstrap-icons/font/bootstrap-icons.css');
+
+$(document).ready(function() {
+    console.log('ready');
+});
+
+// import '@symfony/autoimport';
+
+// Registers Stimulus controllers from controllers.json and in the controllers/ directory
+// export const app = startStimulusApp(require.context('./controllers', true, /\.(j|t)sx?$/));
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
