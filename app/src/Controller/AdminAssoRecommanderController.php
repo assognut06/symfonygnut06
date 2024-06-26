bafc3e4e747f4d847ce9c995777837360df7970e
Gerald Col <gnut@gnut.eu>
2024-06-26 02:13:50 +0200

    Mise en place du javascript de remplacement des images en 404

--- app/src/Controller/AdminAssoRecommanderController.php
+++ app/src/Controller/AdminAssoRecommanderController.php
@@ -1,16 +1,29 @@
 <?php
 
 namespace App\Controller;
-
+use App\Form\AssoRecommanderType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request; // Add this line
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Service\PaginationService;
 use App\Entity\AssoRecommander;
+use App\Repository\AssoRecommanderRepository;
+use App\Service\AssoRecommanderService;
 
+#[Route('/admin')]
 class AdminAssoRecommanderController extends AbstractController
 {
-    #[Route('/admin/asso/recommander/{page}', name: 'app_admin_asso_recommander', defaults: ['page' => 1])]
+    private $assoRecommanderService;
+    private $assoRecommanderRepository;
+
+    public function __construct(AssoRecommanderService $assoRecommanderService, AssoRecommanderRepository $assoRecommanderRepository)
+    {
+        $this->assoRecommanderService = $assoRecommanderService;
+        $this->assoRecommanderRepository = $assoRecommanderRepository;
+    }
+    
+    #[Route('/asso/recommander/liste/{page}', name: 'app_admin_asso_recommander', defaults: ['page' => 1])]
     public function index(PaginationService $paginationService, int $page): Response
     {
         $pagination = $paginationService->getPaginatedData(AssoRecommander::class, $page);
@@ -20,6 +33,41 @@ class AdminAssoRecommanderController extends AbstractController
             'total' => $pagination['total'],
             'pages' => $pagination['pages'],
             'page' => $pagination['current_page'],
+        ]);
+    }
+    
+
+    #[Route('/asso/recommander/new', name: 'app_asso_recommander_new')]
+    public function new(Request $request): Response
+    {
+        // $assoRecommander = new AssoRecommander();
+        $form = $this->createForm(AssoRecommanderType::class);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $organizationSlug = $form->get('organizationSlug')->getData();
+
+            // Vérifier si l'organizationSlug existe déjà
+            if ($this->assoRecommanderRepository->existsByOrganizationSlug($organizationSlug)) {
+                // Ajouter un message flash pour informer l'utilisateur
+                $this->addFlash('danger', 'Le slug de l\'organisation existe déjà.');
+                return $this->redirectToRoute('app_asso_recommander_new');
+            } else {
+
+                $data = $this->assoRecommanderService->updateAssoRecommanderFromApi($organizationSlug);
+
+                if ($data) {
+                    $this->addFlash('success', 'L\'association a bien été recommandée !');
+                } else {
+                    $this->addFlash('danger', 'L\'association n\'a pas été trouvée !');
+                }
+
+                return $this->redirectToRoute('app_asso_recommander_new');
+            }
+        }
+
+        return $this->render('admin/asso_recommander/new.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 }
--- app/src/Controller/AssoRecommanderController.php
+++ app/src/Controller/AssoRecommanderController.php
@@ -2,7 +2,6 @@
 
 namespace App\Controller;
 
-use App\Form\AssoRecommanderType;
 use App\Service\AssoRecommanderService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -27,7 +26,7 @@ class AssoRecommanderController extends AbstractController
         $this->helloAssoApiService = $helloAssoApiService;
     }
 
-    #[Route('/asso/recommander/{page}', name: 'app_asso_recommander', defaults: ['page' => 1])]
+    #[Route('/asso/recommander/liste/{page}', name: 'app_asso_recommander', defaults: ['page' => 1])]
     public function index(PaginationService $paginationService, int $page): Response
     {
         $pagination = $paginationService->getPaginatedData(AssoRecommander::class, $page);
@@ -71,41 +70,6 @@ class AssoRecommanderController extends AbstractController
         return $this->render('billetteries\detail.html.twig', [
             'data_actu' => $data_form,
             'googleMapsApiKey' => $googleMapsApiKey,
-        ]);
-    }
-
-
-    #[Route('/admin/asso/recommander/new', name: 'app_asso_recommander_new')]
-    public function new(Request $request): Response
-    {
-        // $assoRecommander = new AssoRecommander();
-        $form = $this->createForm(AssoRecommanderType::class);
-
-        $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid()) {
-            $organizationSlug = $form->get('organizationSlug')->getData();
-
-            // Vérifier si l'organizationSlug existe déjà
-            if ($this->assoRecommanderRepository->existsByOrganizationSlug($organizationSlug)) {
-                // Ajouter un message flash pour informer l'utilisateur
-                $this->addFlash('danger', 'Le slug de l\'organisation existe déjà.');
-                return $this->redirectToRoute('app_asso_recommander_new');
-            } else {
-
-                $data = $this->assoRecommanderService->updateAssoRecommanderFromApi($organizationSlug);
-
-                if ($data) {
-                    $this->addFlash('success', 'L\'association a bien été recommandée !');
-                } else {
-                    $this->addFlash('danger', 'L\'association n\'a pas été trouvée !');
-                }
-
-                return $this->redirectToRoute('app_asso_recommander_new');
-            }
-        }
-
-        return $this->render('admin/asso_recommander/new.html.twig', [
-            'form' => $form->createView(),
         ]);
     }
 }
