a01c98299a0529a62b5db75f98520404e01ef96d
Gerald Col <gnut@gnut.eu>
2024-06-27 19:02:49 +0200

    Changement de l'email d'envoie

--- app/src/Controller/RegistrationController.php
+++ app/src/Controller/RegistrationController.php
@@ -55,7 +55,7 @@ class RegistrationController extends AbstractController
             // generate a signed url and email it to the user
             $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
                 (new TemplatedEmail())
-                    ->from(new Address('gnut@gnut06.org', 'Gnut 06'))
+                    ->from(new Address('gnut@gnut.eu', 'Gnut 06'))
                     ->to($user->getEmail())
                     ->subject('Please Confirm your Email')
                     ->htmlTemplate('registration/confirmation_email.html.twig')
