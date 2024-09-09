65ad3dd7e4078c58602da21b6fbc688a00d75c7f
Gerald Col <gnut@gnut.eu>
2024-09-09 11:19:18 +0200

    Mise en place du service api FrameVR

--- app/src/Controller/MetaversController.php
+++ app/src/Controller/MetaversController.php
@@ -5,14 +5,32 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Service\ApiFrameVrService;
 
+#[Route('/metavers')]
 class MetaversController extends AbstractController
 {
-    #[Route('/metavers', name: 'app_metavers')]
+    private $apiFrameVrService;
+
+    public function __construct(ApiFrameVrService $apiFrameVrService)
+    {
+        $this->apiFrameVrService = $apiFrameVrService;
+    }
+
+    #[Route('/', name: 'app_metavers')]
     public function index(): Response
     {
         return $this->render('metavers/index.html.twig', [
             'controller_name' => 'MetaversController',
         ]);
     }
+
+    #[Route('/frame/{idFrame}', name: 'app_metavers_ohme')]
+    public function someEndpoint(string $idFrame): Response
+    {
+        $data = $this->apiFrameVrService->getSomeData($idFrame, 'frame');
+
+        return $this->json($data);
+    }
+
 }
