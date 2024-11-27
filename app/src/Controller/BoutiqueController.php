52e60cd617a3ae8af68ba08b60ab6f728706231e
Gerald Col <gnut@gnut.eu>
2024-11-27 21:48:31 +0100

    Innova

--- app/src/Controller/BoutiqueController.php
+++ app/src/Controller/BoutiqueController.php
@@ -6,13 +6,21 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+#[Route('/boutique')]
 class BoutiqueController extends AbstractController
 {
-    #[Route('/boutique', name: 'app_boutique')]
+    #[Route('/', name: 'app_boutique')]
     public function index(): Response
     {
         return $this->render('boutique/index.html.twig', [
             'controller_name' => 'BoutiqueController',
+        ]);
+    }
+    #[Route('/innova', name: 'app_innova')]
+    public function innova(): Response
+    {
+        return $this->render('innova/index.html.twig', [
+            'controller_name' => 'InnovaController',
         ]);
     }
 }
