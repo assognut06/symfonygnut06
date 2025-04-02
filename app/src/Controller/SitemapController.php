60646714bb2ea5f86986d4929bc9e011a19c62b8
Gerald Col <gnut@gnut.eu>
2025-04-02 10:03:05 +0200

    correction pour sitemap.xml

--- app/src/Controller/SitemapController.php
+++ app/src/Controller/SitemapController.php
@@ -5,26 +5,28 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class SitemapController extends AbstractController
 {
-    #[Route('/sitemap', name: 'sitemap', defaults: ['_format' => 'xml'])]
+    #[Route('/sitemap.xml', name: 'sitemap', defaults: ['_format' => 'xml'])]
     public function index(): Response
     {
-        // Liste des URLs à inclure dans le sitemap
-        $urls = [
-            ['loc' => $this->generateUrl('app_home', [], true), 'priority' => '1.0'],
-            ['loc' => $this->generateUrl('app_adhesion', [], true), 'priority' => '0.9'],
-            ['loc' => $this->generateUrl('app_don', [], true), 'priority' => '0.9'],
-            ['loc' => $this->generateUrl('app_metavers', [], true), 'priority' => '0.8'],
-            ['loc' => $this->generateUrl('app_agent_handicap', [], true), 'priority' => '0.8'],
-            ['loc' => $this->generateUrl('app_contact', [], true), 'priority' => '0.8'],
-            ['loc' => $this->generateUrl('app_a_propos', [], true), 'priority' => '0.7'],
-            ['loc' => $this->generateUrl('app_presse', [], true), 'priority' => '0.7'],
-            ['loc' => $this->generateUrl('app_billetteries', [], true), 'priority' => '0.7'],
-            ['loc' => $this->generateUrl('app_asso_recommander', [], true), 'priority' => '0.7'],
-            ['loc' => $this->generateUrl('app_mention_legales', [], true), 'priority' => '0.2'],
-            ['loc' => $this->generateUrl('app_login', [], true), 'priority' => '0.8'],
+         // Liste des URLs à inclure dans le sitemap
+         $urls = [
+            ['loc' => $this->generateUrl('app_home', [], UrlGeneratorInterface::ABSOLUTE_URL), 'priority' => '1.0'],
+            ['loc' => $this->generateUrl('app_adhesion', [], UrlGeneratorInterface::ABSOLUTE_URL), 'priority' => '0.9'],
+            ['loc' => $this->generateUrl('app_don', [], UrlGeneratorInterface::ABSOLUTE_URL), 'priority' => '0.9'],
+            ['loc' => $this->generateUrl('app_metavers', [], UrlGeneratorInterface::ABSOLUTE_URL), 'priority' => '0.8'],
+            ['loc' => $this->generateUrl('app_agent_handicap', [], UrlGeneratorInterface::ABSOLUTE_URL), 'priority' => '0.8'],
+            ['loc' => $this->generateUrl('app_contact', [], UrlGeneratorInterface::ABSOLUTE_URL), 'priority' => '0.8'],
+            ['loc' => $this->generateUrl('app_a_propos', [], UrlGeneratorInterface::ABSOLUTE_URL), 'priority' => '0.7'],
+            ['loc' => $this->generateUrl('app_presse', [], UrlGeneratorInterface::ABSOLUTE_URL), 'priority' => '0.7'],
+            ['loc' => $this->generateUrl('app_billetteries', [], UrlGeneratorInterface::ABSOLUTE_URL), 'priority' => '0.7'],
+            ['loc' => $this->generateUrl('app_asso_recommander', [], UrlGeneratorInterface::ABSOLUTE_URL), 'priority' => '0.7'],
+            ['loc' => $this->generateUrl('app_mention_legales', [], UrlGeneratorInterface::ABSOLUTE_URL), 'priority' => '0.2'],
+            ['loc' => $this->generateUrl('app_login', [], UrlGeneratorInterface::ABSOLUTE_URL), 'priority' => '0.8'],
+            
 
             // Ajoutez d'autres URLs ici
         ];
@@ -37,9 +39,10 @@ class SitemapController extends AbstractController
         // );
 
         // return $response;
-        return $this->render('sitemap/sitemap.xml.Twig', [
-            'urls' => $urls,
-            'Content-Type' => 'application/xml'
-        ]);
+        return new Response(
+            $this->renderView('sitemap/sitemap.xml.twig', ['urls' => $urls]),
+            Response::HTTP_OK,
+            ['Content-Type' => 'application/xml']
+        );
     }
 }
