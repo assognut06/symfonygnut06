f3a83fda63e43da5dfa27980f838e5a8f7bc792c
Skywon <doublet.yann@orange.fr>
2026-01-22 18:08:00 +0100

    Style boutons

--- app/public/js/scripts.js
+++ app/public/js/scripts.js
@@ -6,25 +6,30 @@
     const formSociete = document.getElementById('form-societe');
 
     // Initialisation des boutons et des formulaires
-    btnPhysique.classList.add('btn-primary');
-    btnPhysique.classList.remove('btn-outline-primary');
-    btnSociete.classList.remove('btn-primary');
-    btnSociete.classList.add('btn-outline-secondary');
+    btnPhysique.classList.add('btn-neon');
+    btnPhysique.classList.remove('btn-outline-gradient');
+    btnSociete.classList.remove('btn-neon');
+    btnSociete.classList.add('btn-outline-gradient');
 
     btnPhysique.addEventListener('click', () => {
         formPhysique.style.display = 'block';
         formSociete.style.display = 'none';
-        btnPhysique.classList.add('btn-primary');
-        btnPhysique.classList.remove('btn-outline-primary');
-        btnSociete.classList.remove('btn-primary');
-        btnSociete.classList.add('btn-outline-secondary');
+        btnPhysique.classList.add('btn-neon');
+        btnPhysique.classList.remove('btn-outline-gradient');
+        btnSociete.classList.remove('btn-neon');
+        btnSociete.classList.add('btn-outline-gradient');
     });
 
     btnSociete.addEventListener('click', () => {
         formSociete.style.display = 'block';
         formPhysique.style.display = 'none';
-        btnSociete.classList.add('btn-primary');
-        btnSociete.classList.remove('btn-outline-secondary');
-        btnPhysique.classList.remove('btn-primary');
-        btnPhysique.classList.add('btn-outline-primary');
+        btnSociete.classList.add('btn-neon');
+        btnSociete.classList.remove('btn-outline-gradient');
+        btnPhysique.classList.remove('btn-neon');
+        btnPhysique.classList.add('btn-outline-gradient');
     });
+
+
+
+
+    //Ajouter btn-neon et btn-outline-gradient aux boutons de navigation
